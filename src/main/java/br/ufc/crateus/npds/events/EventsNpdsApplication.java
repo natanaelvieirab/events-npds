bd0cd4716f61834f0315f03c43c21d223058c0fe
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-19 15:17:04 -0300

    refactor(Main): removendo anotação @EntityScan

--- src/main/java/br/ufc/crateus/npds/events/EventsNpdsApplication.java
+++ src/main/java/br/ufc/crateus/npds/events/EventsNpdsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = "model")
 public class EventsNpdsApplication {
 
 	public static void main(String[] args) {
