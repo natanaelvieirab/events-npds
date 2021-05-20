a269caf9261d6ce62ed1c0ca24e294dfbbaf008e
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-20 16:05:28 -0300

    feat(ScheduleRepository): adicionado método findByEvent

--- src/main/java/br/ufc/crateus/npds/events/repository/ScheduleRepository.java
+++ src/main/java/br/ufc/crateus/npds/events/repository/ScheduleRepository.java
@@ -1,11 +1,19 @@
 package br.ufc.crateus.npds.events.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import br.ufc.crateus.npds.events.models.Event;
 import br.ufc.crateus.npds.events.models.Schedule;
 
 @Repository
 public interface ScheduleRepository  extends CrudRepository<Schedule, Integer>{
 
+
+	List<Schedule> findByEvent(Event event, Pageable pageable);
+	
+	
 }
