8d9e75586cf2c409794f40b182379ba51285856f
Natanael Vieira <natanael.vieira.barboza@gmail.com>
2021-05-24 16:22:38 -0300

    refactor(JWT/Service): removendo classe anônima User

--- src/main/java/br/ufc/crateus/npds/events/service/JwtUserDetailsService.java
+++ src/main/java/br/ufc/crateus/npds/events/service/JwtUserDetailsService.java
@@ -12,37 +12,13 @@ import org.springframework.security.core.userdetails.User;
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 
-//	@Autowired
-//    private UserService userService;
-	
-	class UserSystem {
-		private String username;
-		private String password;
-		
-		UserSystem(String username){
-			this.username = username;
-		}
-		public String getUsername() {
-			return username;
-		}
-		public void setUsername(String username) {
-			this.username = username;
-		}
-		public String getPassword() {
-			return password;
-		}
-		public void setPassword(String password) {
-			this.password = password;
-		}
-		
-	}
+
 	
 	@Override
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		 
-		UserSystem user = new UserSystem(email); 
 		
 		// ATENÇÃO: o User é uma classe do pacote SpringFramework > Security > UserDetails
-		return new User(user.getUsername(), "1234", new ArrayList<>());
+		return new User("admin", "admin", new ArrayList<>());
 	}
 }
